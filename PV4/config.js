e19d6c17e051bf76dcbfa216353955db94741bd1
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2022-11-10 16:23:19 +0100

    Creación de PV4

---
+++ PV4/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+
+    mysqlConfig: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "tareas"
+    },
+
+    port: 3000
+}
