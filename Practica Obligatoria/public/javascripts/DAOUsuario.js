a413b6ecf6f0c3949d4e3795e545df3686e2937b
RubnMC <rubema06@ucm.es>
2022-11-24 16:21:07 +0100

    daoUsuario insert user

--- Practica Obligatoria/public/javascripts/DAOUsuario.js
+++ Practica Obligatoria/public/javascripts/DAOUsuario.js
@@ -1,65 +1,36 @@
 "use strict";
 
-class DAOUsers {
+class DAOUsuario {
     constructor(pool) {
         this.pool = pool;
     }
 
-    isUserCorrect(user, callback) {
+    creatUser(user, callback) {
         this.pool.getConnection(function (err, connection) {
             if (err) {
                 callback(new Error("Error de conexión a la base de datos"));
             }
             else {
+                let userRol;
                 if (user.rol === true) {
-                    connection.query("INSERT INTO UCM_AW_CAU_USU_Usuarios(nombre, correo, rol, perfilUniversitario) VALUES (?, ?, 'Técnico', ?)",
-                        [user.nombre, user.correo, user.perfilUniversitario],
-                        function (err, rows) {
-                            if (err) {
-                                callback(new Error("Error de acceso a la base de datos, insertar tarea"));
-                            }
-                            else {
-                                if (rows.length === 0) {
-                                    callback(null, "No se ha podido añadir la tarea"); //no está el usuario con el password proporcionado
-                                }
-                                else {
-                                    let idUsuario = rows.insertId;
-
-                                    connection.query("INSERT INTO UCM_AW_CAU_CON_Contrasenas(idUsuario, password) VALUES (?, ?)",
-                                        [idUsuario, user.password],
-                                        function (err, rows) {
-                                            if (err) {
-                                                callback(new Error("Error de acceso a la base de datos, insertar tarea"));
-                                            }
-                                            else {
-                                                if (rows.length === 0) {
-                                                    callback(null, "No se ha podido añadir la tarea"); //no está el usuario con el password proporcionado
-                                                }
-                                                else {
-                                                     //COMPLETADO
-                                                }
-                                            }
-                                        });
-
-                                }
-                            }
-                        });
-
+                    userRol = "Técnico";
                 } else {
-                    connection.query("INSERT INTO UCM_AW_CAU_USU_Usuarios(nombre, correo, rol, perfilUniversitario) VALUES (?, ?, 'Usuario', ?)",
-                        [user.nombre, user.correo, user.perfilUniversitario],
-                        function (err, rows) {
-                            if (err) {
-                                callback(new Error("Error de acceso a la base de datos, insertar tarea"));
+                    userRol = "Usuario";
+                }
+                connection.query("INSERT INTO UCM_AW_CAU_USU_Usuarios(nombre, nombrePublico, correo, rol, perfilUniversitario) VALUES (?, ?, ?, '" + userRol + "', ?)",
+                    [user.nombre, user.nombrePublico, user.correo, user.perfilUniversitario],
+                    function (err, rows) {
+                        if (err) {
+                            callback(new Error("Error de acceso a la base de datos, insertar tarea"));
+                        }
+                        else {
+                            if (rows.length === 0) {
+                                callback(null, "No se ha podido añadir la tarea"); //no está el usuario con el password proporcionado
                             }
                             else {
-                                if (rows.length === 0) {
-                                    callback(null, "No se ha podido añadir la tarea"); //no está el usuario con el password proporcionado
-                                }
-                                else {
-                                    let idUsuario = rows.insertId;
+                                let idUsuario = rows.insertId;
 
-                                    connection.query("INSERT INTO UCM_AW_CAU_CON_Contrasenas(idUsuario, password) VALUES (?, ?)",
+                                connection.query("INSERT INTO UCM_AW_CAU_CON_Contrasenas(idUsuario, password) VALUES (?, ?)",
                                     [idUsuario, user.password],
                                     function (err, rows) {
                                         if (err) {
@@ -70,15 +41,13 @@ class DAOUsers {
                                                 callback(null, "No se ha podido añadir la tarea"); //no está el usuario con el password proporcionado
                                             }
                                             else {
-                                                 //COMPLETADO
+                                                //COMPLETADO
                                             }
                                         }
                                     });
-                                }
                             }
-                        });
-
-                }
+                        }
+                    });
 
             }
         }
