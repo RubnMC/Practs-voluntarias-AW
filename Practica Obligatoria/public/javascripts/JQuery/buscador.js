4b6a6b39ec393ee9fbb176e4b79752e2c9a9f132
RubnMC <rubema06@ucm.es>
2022-12-11 17:18:55 +0100

    ahora se puede buscar con el enter

--- Practica Obligatoria/public/javascripts/JQuery/buscador.js
+++ Practica Obligatoria/public/javascripts/JQuery/buscador.js
@@ -1,6 +1,12 @@
 //Buscador, se encuentra en la cabecera
 
 $(document).ready(function () {
+    $("#textoBuscar").keypress(function (k) {
+        if (k.which == 13) {
+            $('#botonBuscar').click();
+        }
+    })
+
     $("#botonBuscar").click(function () {
       $('#tablaAvisos > tbody  > tr').each(function(index, tr) {
           $(tr).show()
