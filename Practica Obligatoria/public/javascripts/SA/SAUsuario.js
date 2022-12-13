b35cbfa0a7a12b7a263c219589d0616cb10b9041
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2022-12-13 20:59:18 +0100

    Comprobación contraseña reactivada y tablas a punto.

--- Practica Obligatoria/public/javascripts/SA/SAUsuario.js
+++ Practica Obligatoria/public/javascripts/SA/SAUsuario.js
@@ -8,15 +8,18 @@ class SAUsuario {
     }
 
     crearUsuario(user, callback) {
+
+        let contrasenaSegura = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
         if (user.password !== user.password_2) {
             callback(new Error("Las contraseñas no coinciden"));
         }
         else if (user.numTecnico && !(/\d{4}\-[A-Z]{3}/.test(user.numTecnico))) {
             callback(new Error("Formato de num.empleado incorrecto (0000-ABC)"));
         }
-        // if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/.test(user.password)) {
-        //     callback(new Error("La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, una minúscula, una mayuscula y un caracter especial"));
-        // }
+        else if (!(contrasenaSegura.test(user.password))) {
+            callback(new Error("La contraseña debe tener entre 8 y 16 caracteres, al menos un dígito, una minúscula, una mayuscula y un caracter especial"));
+        }
         else {
             this.daoUser.createUser(user, function (err, res) {
                 if (err) {
